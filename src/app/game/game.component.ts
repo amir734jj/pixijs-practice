5f9c66490ebd4cc1dbd4bcb835cc294b3945ff80
Seyedamirhossein Hesamian <hesamian@uwm.edu>
2019-10-15 01:12:37 -0500

    started work

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import * as PIXI from 'pixi.js';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss']
+})
+export class GameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    // The application will create a renderer using WebGL, if possible,
+    // with a fallback to a canvas render. It will also setup the ticker
+    // and the root stage PIXI.Container
+    const app = new PIXI.Application();
+
+    // The application will create a canvas element for you that you
+    // can then insert into the DOM
+    document.body.appendChild(app.view);
+
+    // load the texture we need
+    app.loader.add('bunny', 'assets/bunny.png').load((loader, resources) => {
+      // This creates a texture from a 'bunny.png' image
+      const bunny = new PIXI.Sprite(resources.bunny.texture);
+
+      // Setup the position of the bunny
+      bunny.x = app.renderer.width / 2;
+      bunny.y = app.renderer.height / 2;
+
+      // Rotate around the center
+      bunny.anchor.x = 0.5;
+      bunny.anchor.y = 0.5;
+
+      // Add the bunny to the scene we are building
+      app.stage.addChild(bunny);
+
+      // Listen for frame updates
+      app.ticker.add(() => {
+        // each frame we spin the bunny around a bit
+        bunny.rotation += 0.01;
+
+        bunny.x += 1;
+      });
+    });
+  }
+
+}
